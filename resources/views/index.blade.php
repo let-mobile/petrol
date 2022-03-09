a6d43a36fde1a73eb7c6453be6ccf7c42db53d7a
Aamir Burki <aamirkb2017@gmail.com>
2022-03-09 23:58:08 +0500

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,21 @@
-<nav class="main-header bg-dark">
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My Blog | Log in</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="{{ asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('assets/dist/css/adminlte.min.css') }}">
+</head>
+
+<body>
     <ul class="navbar-nav">
         <li class="nav-item">
             <form method="POST" action="{{ route('logout') }}">
@@ -6,6 +23,6 @@
                 <button type="submit" class="btn btn-primary">Logout</button>
             </form>
         </li>
-      </ul>
-</nav>
-<h2>My Dashboard</h2>
+    </ul>
+    <h2>My Dashboard</h2>
+</body>
