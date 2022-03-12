8364f52efb40a6caec712a4d6c3265d2653705ad
Aamir Burki <aamirkb2017@gmail.com>
2022-03-13 00:26:51 +0500

    remove namespacespace and insert spatie class in config.app

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ForgotPasswordController;
