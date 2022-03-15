41bce7a0bf9f5b3d2686cff770cba91b429bebbf
Hamza Saleem <hamzasaleemsepra786@gmail.com>
2022-03-15 23:43:27 +0500

    wert

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\RoleController;
-use App\Http\Controllers\UnitController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ForgotPasswordController;
 
@@ -29,16 +26,26 @@ Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
 Route::resource('roles',App\Http\Controllers\RoleController::class);
 
 //.......User.......//
-Route::get('create_user',[UserController::class,'create'])->name('add_user');
-Route::post('store',[UserController::class,'store']);
+Route::resource('users',App\Http\Controllers\UserController::class);
 
-//.......Unit.......//
-Route::get('create_unit',[UnitController::class,'create'])->name('create_unit');
-Route::get('all-units',[UnitController::class,'index'])->name('all-units');
-Route::post('store_unit',[UnitController::class,'store'])->name('store_unit');
-Route::get('unit/edit/{id}',[UnitController::class,'edit'])->name('unit/edit');
-Route::post('unit/update/{id}',[UnitController::class,'update'])->name('unit/update/{id}');
-Route::get('unit/destroy/{id}',[UnitController::class,'destroy'])->name('unit/destroy/{id}');
+//.......Unit.......//\
+
+
+Route::resource('units',App\Http\Controllers\UnitController::class);
+
+
+
+
+// Route::get('units',[UnitController::class,'create'])->name('units');
+
+// Route::get('all-units',[UnitController::class,'index'])->name('all-units');
+// Route::post('store_unit',[UnitController::class,'store'])->name('store_unit');
+// Route::get('unit/edit/{id}',[UnitController::class,'edit'])->name('unit/edit');
+// Route::post('unit/update/{id}',[UnitController::class,'update'])->name('unit/update/{id}');
+// Route::get('unit/destroy/{id}',[UnitController::class,'destroy'])->name('unit/destroy/{id}');
+
+
+
 
 
 
